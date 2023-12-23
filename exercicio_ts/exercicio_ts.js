9d5244ba91f02251cbb5d8f330939cd64ff8b33c
Bruno <brunodossantosportela@gmail.com>
2023-12-23 10:01:21 -0300

    Adicionando arquivos do exercicio_ts

---
+++ exercicio_ts/exercicio_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(a, b) {
+    return console.log(a * b);
+}
+function saudacao(nome) {
+    return console.log(`Olá ${nome}`);
+}
+multiplicacao(22, 12);
+saudacao('Matheus');
---
+++ exercicio_ts/exercicio_ts.ts
@@ -0,0 +1,10 @@
+function multiplicacao(a: number, b: number) {
+    return console.log(a * b)
+}
+
+function saudacao(nome: string) {
+    return console.log(`Olá ${nome}`)
+}
+
+multiplicacao(22, 12);
+saudacao('Matheus')
